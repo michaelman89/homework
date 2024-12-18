22b52ec4b039e0a8eaaf4a1c9acaa5678c29bc65
michaelman89 <manapov.md@phystech.edu>
2024-12-18 03:51:48 +0300

    Add files via upload

---
+++ seminar7/sem7num1.py
@@ -0,0 +1,34 @@
+class Vector():
+    def __init__(self,x, y, z):
+        assert isinstance(x, int) or isinstance(x, float)
+        assert isinstance(x, int) or isinstance(y, float)
+        assert isinstance(x, int) or isinstance(z, float)
+        self.x=x
+        self.y=y
+        self.z=z
+    def __abs__(self):
+        return(self.x**2+self.y**2+self.z**2)**0.5
+    def __radd__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x+other.x, self.y+other.y, self.z+other.z)
+        if isinstance(other, int):
+            return Vector(self.x+other, self.y+other, self.z+other)
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x+other.x,self.y+other.y,self.z+other.z)
+        elif isinstance(other, int) or isinstance(other, float):
+            return Vector(self.x+other,self.y+other,self.z+other)
+    def __sub__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x-other.x, self.y-other.y, self.z-other.z)
+        elif isinstance(other, int) or isinstance(other, float):
+            return Vector(self.x-other,self.y-other,self.z-other)
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            return self.x*other.x + self.y*other.y + self.z*other.z
+        if isinstance(other, int) or isinstance(other, float):
+            return Vector(self.x*other, self.y*other, self.z*other)
+    def __str__(self):
+        return f'x = {self.x} y = {self.y} z = {self.z}'
+x,y,z=map(float, input().split())
+print(Vector(x,y,z)*5)
